7db731f8292b19a8e079c655139446f92d555bce
Ryougi-Shio <83452750+Ryougi-Shio@users.noreply.github.com>
2021-05-29 14:22:37 +0800

    Update SafeScene.cpp

--- Classes/Scene/SafeScene.cpp
+++ Classes/Scene/SafeScene.cpp
@@ -1,4 +1,3 @@
-<<<<<<< main
 #include"SafeScene.h"
 #include"cocos2d.h"
 #include"json.h"
@@ -8,59 +7,38 @@ bool SafeScene::init()
 {
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	auto origin = Director::getInstance()->getVisibleOrigin();
-	
 
-<<<<<<< main
-	//����ͼƬ����
-=======
-	//����ͼƬ����
->>>>>>> main
+
+	//背景图片精灵
 	auto backgroundSprite = Sprite::create("background/SafeScene.png");
 	backgroundSprite->setPosition(origin.x + backgroundSprite->getContentSize().width / 2, origin.y + backgroundSprite->getContentSize().height / 2);
 	this->addChild(backgroundSprite,1);
 
 
 
-<<<<<<< main
-	//���bgm�Լ���tiledmap
-=======
-	//���bgm�Լ���tiledmap
->>>>>>> main
+	//更改bgm以及绑定tiledmap
 	getmusicManager()->changeMusic("bgm/Room.mp3");
 	map = TMXTiledMap::create("maps/SafeScene.tmx");
 	bindTiledMap(map);
 
-<<<<<<< main
-	//���ð�ť
-=======
-	//���ð�ť
->>>>>>> main
+	//设置按钮
 	auto settings = MenuItemImage::create("ui/settings.png", "ui/settings.png", [&](Ref* sender) {
 		getmusicManager()->effectPlay("effect/button.mp3");
-		getmusicManager()->menu(this);
-	});
-	//λ��λ�����Ͻ�
-	settings->setPosition(visibleSize.width - settings->getContentSize().width / 2, visibleSize.height - settings->getContentSize().height/2);
-
-	//�˵�
+@@ -21,36 +32,51 @@ bool SafeScene::init()
+	//菜单
 	auto menu = Menu::create(settings, nullptr);
 	menu->setPosition(Vec2::ZERO);
+	addChild(menu);
 	addChild(menu,5);
 
-	//��Ҵ���
+	//玩家创建
 	player = Player::create();
 	player->setPosition(64 * 4 + 32, 64 * 4 + 32);
-<<<<<<< main
-	player->getplayermove()->bindMap(map);//PlayerMove������ͼ��
+	addChild(player);
+	player->getplayermove()->bindMap(map);//PlayerMove跟这个地图绑定
 	this->addChild(player,2);
 
-	//�����Ŵ���
-=======
-	player->getplayermove()->bindMap(map);//PlayerMove������ͼ��
-	this->addChild(player,2);
-
-	//�����Ŵ���
->>>>>>> main
+	//传送门创建
 	safeGate = Gate::create();
 	safeGate->setPosition(64*9.45,64*10);
 	safeGate->bindPlayer(player);
@@ -68,23 +46,21 @@ bool SafeScene::init()
 	safeGate->bindDestination(this);
 	this->addChild(safeGate, 5);
 
-	////eventlistener,���̼��������ƶ�����
+	////eventlistener,键盘监听，用于移动人物
 	auto myKeyListener = EventListenerKeyboard::create();
-	myKeyListener->onKeyPressed = [=](EventKeyboard::KeyCode keycode, cocos2d::Event* event)//���̰���ʱ��Ӧ
+	myKeyListener->onKeyPressed = [=](EventKeyboard::KeyCode keycode, cocos2d::Event* event)//键盘按下时响应
 	{
 		//CCLOG("keycode%d", keycode);
-<<<<<<< main
-		player->getplayermove()->TrueKeyCode(keycode);//PlayerMove��keyMap�Ķ�Ӧ������true
-=======
-		player->getplayermove()->TrueKeyCode(keycode);//PlayerMove��keyMap�Ķ�Ӧ������true
->>>>>>> main
+		player->TrueKeyCode(keycode);
+		player->getplayermove()->TrueKeyCode(keycode);//PlayerMove里keyMap的对应键码置true
 
 	};
 
-	myKeyListener->onKeyReleased = [=](EventKeyboard::KeyCode keycode, cocos2d::Event* event)//�����ɿ�ʱ��Ӧ
+	myKeyListener->onKeyReleased = [=](EventKeyboard::KeyCode keycode, cocos2d::Event* event)//键盘松开时响应
 	{
 
 		//CCLOG("keyboard is released,code is %d", keycode);
+		player->FalseKeyCode(keycode);
 		player->getplayermove()->FalseKeyCode(keycode);
 	};
 
@@ -97,23 +73,11 @@ bool SafeScene::init()
 
 void SafeScene::update(float dt)
 {
-<<<<<<< main
-	//����Player��update��Player��update�ٵ���PlayMove��move�����ֹ���ޣ�
+	//调用Player的update，里面有运动的实现
+	//调用Player的update，Player的update再调用PlayMove的move函数（禁止套娃）
 	//isWall(player->getPositionX(), player->getPositionY())
-	//jsonʹ��ʾ��
-=======
-	//����Player��update��Player��update�ٵ���PlayMove��move�����ֹ���ޣ�
-	//isWall(player->getPositionX(), player->getPositionY())
-	//jsonʹ��ʾ��
->>>>>>> main
+	//json使用示例
 	player->update(dt);
+} 
 	safeGate->update(dt);
 }
-
-<<<<<<< main
-=======
-=======
-
->>>>>>> main
-
->>>>>>> main
