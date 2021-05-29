4f504aacb9aff965f9586b8ca6ee58d19dafdbe9
Ryougi-Shio <83452750+Ryougi-Shio@users.noreply.github.com>
2021-05-29 14:16:40 +0800

    Update SafeScene.h

--- Classes/Scene/SafeScene.h
+++ Classes/Scene/SafeScene.h
@@ -1,1 +1,29 @@
+<<<<<<< main
+#pragma once
+#include"NormalScene.h"
+#include"Player.h"
+#include"Gate.h"
+USING_NS_CC;
+/*
+* ��
+* ��
+* ������
+* ���
+*/
+class SafeScene : public NormalScene
+{
+public:
+	
+	virtual bool init();
+	virtual void update(float dt);
+	bool isWall(float Px, float Py);
+	CREATE_FUNC(SafeScene);
+private:
+	TMXTiledMap* map;
+	Player* player;
+	Gate* safeGate;
+};
+=======
+
+>>>>>>> main
 
