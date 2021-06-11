5272cf6ff932c6db78fabc5dcdcf41c74f8d45cb
Ryougi-Shio <83452750+Ryougi-Shio@users.noreply.github.com>
2021-06-11 20:04:32 +0800

    Delete Resources directory

--- Resources/maps/tiles1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="tiles1" tilewidth="28" tileheight="28" tilecount="49" columns="7">
- <image source="tiles1.png" width="196" height="196"/>
-</tileset>
