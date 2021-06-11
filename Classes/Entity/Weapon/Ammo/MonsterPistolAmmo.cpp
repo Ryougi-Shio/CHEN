5f55fd64ac97e528c07044a04de7fa244cd21cb2
Ryougi-Shio <83452750+Ryougi-Shio@users.noreply.github.com>
2021-06-11 20:04:18 +0800

    Delete Classes directory

--- Classes/Entity/Weapon/Ammo/MonsterPistolAmmo.cpp
+++
@@ -1,8 +0,0 @@
-#include"MonsterPistolAmmo.h"
-
-bool MonsterPistolAmmo::init()
-{
-	bindSprite(Sprite::create("Weapon/Ammo/monster_bullet_2.png"));
-
-	return 1;
-}
--- Classes/Entity/Weapon/Ammo/PistolAmmo.cpp
+++
@@ -1,8 +0,0 @@
-#include"PistolAmmo.h"
-
-bool PistolAmmo::init()
-{
-	bindSprite(Sprite::create("Weapon/Ammo/shotgunbullet.png"));
-	
-	return 1;
-}
